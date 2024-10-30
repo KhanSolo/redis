6da865db5aaeeca739b07b50e372f07515f67d5f
Konstantin <drpdrp@mail.ru>
2024-10-31 00:01:56 +0300

    Output reqest

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::str;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    println!("accepted new connection");
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    {
+        let string = str::from_utf8(&buffer).expect("Our bytes should be valid utf8");
+        println!("{string}");
+        //println!("{:?}", &buffer);
+    }
+
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
